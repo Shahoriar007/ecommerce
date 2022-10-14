395300388e797d8ad13d5561401a926392cbbe0f
Shahoriar007 <shahoriar.fahim@gmail.com>
2022-10-14 21:22:23 +0600

    feat: add reset password section with frontend

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,36 +1,59 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('frontend.main_master')
+@section('content')
 
-        <x-jet-validation-errors class="mb-4" />
+<div class="breadcrumb">
+	<div class="container">
+		<div class="breadcrumb-inner">
+			<ul class="list-inline list-unstyled">
+				<li><a href="home.html">Home</a></li>
+				<li class='active'>Reset Password</li>
+			</ul>
+		</div><!-- /.breadcrumb-inner -->
+	</div><!-- /.container -->
+</div><!-- /.breadcrumb -->
 
-        <form method="POST" action="{{ route('password.update') }}">
+<div class="body-content">
+	<div class="container">
+		<div class="sign-in-page">
+			<div class="row">
+				<!-- Sign-in -->			
+<div class="col-md-6 col-sm-6 sign-in">
+	<h4 class="">Reset Password</h4>
+	
+
+    <form method="POST" action="{{ route('password.update') }}">
             @csrf
 
             <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
-            </div>
+		<div class="form-group">
+		    <label class="info-title" for="exampleInputEmail1">Email Address <span>*</span></label>
+		    <input type="email" name="email" id="email"  class="form-control unicase-form-control text-input"  >
+		</div>
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+        <div class="form-group">
+		    <label class="info-title" for="exampleInputEmail1">Password <span>*</span></label>
+		    <input type="password" name="password" id="password"  class="form-control unicase-form-control text-input"  >
+		</div>
 
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
+        <div class="form-group">
+		    <label class="info-title" for="exampleInputEmail1">Confirm Password <span>*</span></label>
+		    <input type="password" name="password_confirmation" id="password_confirmation"  class="form-control unicase-form-control text-input"  >
+		</div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Reset Password') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+	  	<button type="submit" class="btn-upper btn btn-primary checkout-page-button">Reset Password</button>
+	</form>					
+</div>
+<!-- Sign-in -->
+
+		
+</div><!-- /.row -->
+		</div><!-- /.sigin-in-->
+<!-- ============================================== BRANDS CAROUSEL ============================================== -->
+
+    @include('frontend.body.brands')
+
+<!-- ============================================== BRANDS CAROUSEL : END ============================================== -->	</div><!-- /.container -->
+</div><!-- /.body-content -->
+
+@endsection
